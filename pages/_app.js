1889c35364e1a95eedd0d9973a72938743c6bd45
marco.filetto <marcoarielf96@gmail.com>
2021-08-15 19:34:37 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
+import React, {useEffect} from 'react';
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    let scroll;
+    import("locomotive-scroll/dist/locomotive-scroll").then((locomotiveModule) => {
+        scroll = new locomotiveModule.default({
+            el: document.querySelector("[data-scroll-container]"),
+            smooth: true,
+            smoothMobile: false,
+            resetNativeScroll: true
+        });
+    });
+
+    // `useEffect`'s cleanup phase
+    return () => scroll.destroy();
+});
+
   return <Component {...pageProps} />
 }
 
